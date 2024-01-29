f677d62b4fc9547745c7fa841ae2ae13de5b4d2a
viktorija <viktorija.kotaite@gmail.com>
2024-01-29 11:21:02 +0200

    button style

--- crud_front/src/pages/student/CreateStudent.jsx
+++ crud_front/src/pages/student/CreateStudent.jsx
@@ -102,7 +102,10 @@ function CreateStudent() {
           )}
         </div>
         <div className="flex items-center justify-center">
-          <button type="submit" className="bg-blue-500">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
             Create
           </button>
         </div>
